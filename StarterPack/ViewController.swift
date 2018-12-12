99ae987f80e863e4a93a336b83a1cc2bdccda4a6
shrashwiniw <shrashwiniw@gmail.com>
2018-12-12 17:56:14 +0545

    Initial Commit

---
+++ StarterPack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarterPack
+//
+//  Created by Insight Workshop on 12/12/18.
+//  Copyright © 2018 Insight Workshop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
